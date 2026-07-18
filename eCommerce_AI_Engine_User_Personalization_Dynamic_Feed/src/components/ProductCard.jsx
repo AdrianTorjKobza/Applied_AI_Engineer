5fb6792d5f754583da584632cfac2a200db168c6
Adrian Torj-Kobza <42949599+AdrianTorjKobza@users.noreply.github.com>
2026-07-18 11:31:42 +0400

    Delete eCommerce_AI_Engine_User_Personalization_Dynamic_Feed directory

--- eCommerce_AI_Engine_User_Personalization_Dynamic_Feed/src/components/ProductCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProductCard({ item }) {
-  return (
-    <div className="p-4 bg-white rounded shadow border-l-4 border-blue-500">
-      <h3 className="font-bold">{item.name}</h3>
-      <p className="text-sm text-gray-600">Match Score: {item.match_score}</p>
-    </div>
-  );
-}
